e1628a207da639f5e855346e5fe4f2f305027016
Ekenayy <ekenayy@gmail.com>
2020-11-18 19:54:18 -0500

    Helper methods created

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
+require 'pry'
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def my_songs
+        Song.all.select {|song| song.artist == self}
+    end 
+    def song_count
+        self.my_songs.count
+    end 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+
+weezy = Artist.create(name: "Lil' Wayne")
+song1= Song.create(title: "Go DJ", artist_id: weezy.id)
+song2 = Song.create(title: "Lil Duffle Bag Boy", artist_id: weezy.id)
+drizzy = Artist.create(name: "Drake")
+song3 = Song.create(title: "You the best", artist_id: drizzy.id)
+
+
+puts weezy.song_count
+puts song2.artist_name
